f7a0a6ace6ef8740803c9a12312e30154209bf37
student <student@ip-172-31-55-118.us-west-2.compute.internal>
2021-06-10 15:29:31 -0700

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -9,25 +9,47 @@ import org.jointheleague.graphical.robot.Robot;
  *    Level 1
  */
 public class Houses {
+	Robot rob = new Robot();
 	
 	public void run() {
 		// Check the recipe to find out what code to put here
-		Robot rob = new Robot();
-		Robot.setWindowSize(500, 750);
-		rob.setY(250);
+		Robot.setWindowSize(5000, 1000);
+		rob.setY(500);
+		rob.setX(0);
 		rob.show();
 		rob.penDown();
 		rob.setSpeed(100);
 		rob.setPenColor(Color.blue );
-	    for(int i= 0;i<3;i++) {
-	    	rob.move(100);
-	    	rob.turn(90);
+		
+		
+			for(int i=0;i<9;i++) {
+				drawHouse("small");
+			
+			}
+			drawHouse("medium");
+			drawHouse("large");
 	    }
-	    	
-	    }
-		void house() {
-		for(int i=0;i<2;i++) {
-			run();
-		}
+		void drawHouse(String size) {
+			 int height = 50;
+			if(size.equalsIgnoreCase("small")) {
+				height = 60;
+			}
+			if(size.equalsIgnoreCase("medium")) {
+				
+				height = 120;
+			}
+			if(size.equalsIgnoreCase("large")) {
+				
+				height = 250;
+			}
+			for(int i= 0;i<2;i++) {
+		    	rob.move(height);
+		    	rob.turn(90);
+		    }
+				rob.move(height);
+		    	rob.turn(-90);
+		    	rob.move(height);
+		    	rob.turn(-90);
+		    	
 	}
 }
--- src/_01_methods/_1_houses/HousesRunner.java
+++ src/_01_methods/_1_houses/HousesRunner.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -11,7 +14,7 @@ public class HousesRunner {
 		
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-
+		
 
 	}
 
